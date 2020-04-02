68b147ea087709fba68374783bfa9718330698c5
Artem Suprunovich <artemususus@gmail.com>
2020-04-02 12:42:04 +0300

    add logic to handle conection status

--- flutter_app/lib/Managers/network_manager.dart
+++ flutter_app/lib/Managers/network_manager.dart
@@ -1,10 +1,13 @@
 import 'dart:convert';
 import 'package:flutter/material.dart';
-import 'package:flutter_app/Models/passanger.dart';
 import 'package:flutter_app/Models/route.dart';
 import 'package:flutter/services.dart' show rootBundle;
+import 'package:connectivity/connectivity.dart';
 
 class NetworkManager {
+  static var _internetConectionEvent;
+  static bool isConected;
+  static Function onConectionLost;
   static Future<String> _loadAsRoutesAsset() async {
     return await rootBundle.loadString('assets/fakeData/routes.json');
   }
@@ -17,9 +20,40 @@ class NetworkManager {
     return routes;
   }
 
-  static Future<List<Passanger>> getPassenger() {}
+  static Future<int> getPassengerCountFromStation() {}
+  static Future<int> updatePassangerCountInCar() {}
 
   static void updateStation() {}
+
+  static void startListenConectionState(Function onConectionLostHandler) {
+    onConectionLost = onConectionLostHandler;
+    _internetConectionEvent = Connectivity()
+        .onConnectivityChanged
+        .listen((ConnectivityResult result) async {
+      print("Connection Status has Changed");
+      isConected = await getConectionStatus();
+      if (!isConected) onConectionLost();
+    });
+  }
+
+  static void removeConectionListener() {
+    _internetConectionEvent.cancel();
+    onConectionLost = null;
+  }
+
+  static Future<bool> getConectionStatus() async {
+    var connectivityResult = await (Connectivity().checkConnectivity());
+    if (connectivityResult == ConnectivityResult.mobile) {
+      print("Connected to Mobile Network");
+      return true;
+    } else if (connectivityResult == ConnectivityResult.wifi) {
+      print("Connected to WiFi");
+      return true;
+    } else {
+      print("Unable to connect. Please Check Internet Connection");
+      return false;
+    }
+  }
 
   static Widget loadWidget() {
     return Center(
--- flutter_app/lib/authorization.dart
+++ flutter_app/lib/authorization.dart
@@ -69,6 +69,8 @@ class AuthorizationState extends State {
 
   @override
   void initState() {
+    NetworkManager.startListenConectionState(
+        () => {print("NOOOOOOOOOOOOOOOOOO!!!!")});
     setState(() {
       currentState = NetworkManager.loadWidget();
     });
@@ -77,6 +79,7 @@ class AuthorizationState extends State {
 
   @override
   void dispose() {
+    NetworkManager.removeConectionListener();
     super.dispose();
   }
 
